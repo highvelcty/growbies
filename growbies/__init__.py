6a97fd094671de1c69635bbfc59ae7fc30d07fdf
Eric Meyer <ericallenmeyer@gmail.com>
2025-01-09 16:45:01 -0700

    Change doc string.

--- growbies/__init__.py
+++ growbies/__init__.py
@@ -1,6 +1,7 @@
 """
-Welcome to Growbies! An artificial intelligence driven closed loop feedback control for growing
-things!
+Welcome to Growbies!
+
+A machine learning driven, closed-loop feedback control for growing plants.
 """
 from .session.session import Session
 
