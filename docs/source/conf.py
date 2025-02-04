65c3da257230d1b950d0921c75034791876ed13c
Eric Meyer <ericallenmeyer@gmail.com>
2025-02-04 10:15:27 -0700

    Switch themes and doc warning cleanup.]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,5 +21,8 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
-# html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
+extensions = ['sphinx_rtd_dark_mode']
+default_dark_mode = True
+
+html_static_path = ['_static']
