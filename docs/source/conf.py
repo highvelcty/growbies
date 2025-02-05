1bbf970f6c5adbed68b783c7e98ca18be8b76165
Eric Meyer <ericallenmeyer@gmail.com>
2025-02-05 11:23:18 -0700

    Update project name.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Eric Meyer (highvelcty)'
+project = 'Growbies'
 copyright = '2025, Eric Meyer'
 author = 'Eric Meyer'
 
