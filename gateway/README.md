b64647956934a07512f7a198d446ab72937e5dcd
Eric Meyer <ericallenmeyer@gmail.com>
2025-03-14 12:25:02 -0600

    Debian packaging - clear all lintian errors and warnings. Incremental improvments to the build.

--- gateway/README.md
+++ gateway/README.md
@@ -1,3 +1,7 @@
 # emey: this is a work in progress
 - Build dependencies
   - podman
+
+`dch` may be helpful in editing the changelog
+`apt-get build-dep .` to install package build dependencies
+`debuild -b` to build binary only without an upstream source
--- gateway/deb/debian/build.sh
+++ gateway/deb/debian/build.sh
@@ -8,9 +8,17 @@ source debian/install_vars.sh
 PATH_GIT_ROOT=$(git rev-parse --show-toplevel)
 PATH_DIST="${PATH_GIT_ROOT}"/dist
 
+# Install growbies-gateway into the virtual environment
 mkdir -p "${PATH_IN_TMP}"
 ${PYTHON} -m venv "${PATH_IN_VENV}"
 source "${PATH_IN_VENV_ACTIVATE}"
 pip install "${PATH_GIT_ROOT}"[BUILD]
 make -C "${PATH_GIT_ROOT}"
 cp "${PATH_DIST}"/*.whl "${PATH_IN_TMP}"
+
+# Install the changelog and compress it, per debian requirements
+dh_installchangelogs
+dh_compress
+
+# Install the copyright
+dh_installdocs
