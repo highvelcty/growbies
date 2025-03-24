06a9ed0d15258d59f6442b09caa85a9783558cc1
Eric Meyer <ericallenmeyer@gmail.com>
2025-03-24 09:18:31 -0600

    Install growbies gateway systemd service.

--- pkg/deb/debian/install.sh
+++ pkg/deb/debian/install.sh
@@ -23,3 +23,5 @@ dh_install "${PATH_DIST}"/*.whl "${PATH_USR_LIB_GROWBIES}"
 dh_install "${PATH_BUILD_PATHS_ENV}" "${PATH_USR_LIB_GROWBIES}"
 dh_install "${PATH_PKG_BASH_SRC_GROWBIES}" "${PATH_USR_BIN}"
 
+# Install systemd service
+dh_installsystemd
